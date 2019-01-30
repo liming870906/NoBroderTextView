088478fcd00e3566741ec1c7f3c0df320af884e0
liming870906 <370322364@qq.com>
2019-01-30 14:50:25 +0800

    优化自定义控件方法。添加MATCH_PARENT及WRAP_CONTENT效果

--- app/src/main/java/com/liming/nobroder/nobrodertextview/MainActivity.kt
+++ app/src/main/java/com/liming/nobroder/nobrodertextview/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.liming.nobroder.nobrodertextview
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.liming.nobroder.nopaddingtextview.views.NoPaddingTextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.findViewById<NoPaddingTextView>(R.id.nptv_name_1)
+            .setText("测试文本：动态加载数据显示效果。动态加载数据显示效果。动态加载数据显示效果。")
     }
 }
--- nopaddingtextview/src/main/java/com/liming/nobroder/nopaddingtextview/views/NoPaddingTextView.java
+++ nopaddingtextview/src/main/java/com/liming/nobroder/nopaddingtextview/views/NoPaddingTextView.java
@@ -12,8 +12,8 @@ import android.util.Log;
 import android.view.ViewGroup;
 
 public class NoPaddingTextView extends AppCompatTextView {
+    //日志标记
     private final String TAG = NoPaddingTextView.class.getSimpleName();
-
     //文本画笔
     private TextPaint textPaint;
     //绘制矩形
@@ -52,53 +52,94 @@ public class NoPaddingTextView extends AppCompatTextView {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         Layout _layout = getLayout();
         if (_layout != null) {
-            Log.e(TAG, "Layout:Wight = " + _layout.getWidth() + " <> Layout_Height = " + _layout.getHeight());
             //获得文本内容文本内容不可以修改,平切判断当前当前内容是否为null
             final String _tvContent = TextUtils.isEmpty(getText()) ? "" : getText().toString();
             //获取文本长度
             final int _tvLenght = _tvContent.length();
             //设置文本宽度
             textPaint.getTextBounds(_tvContent, 0, _tvLenght, rect);
-            Log.e(TAG, "Layout:Rect = " + rect.toString());
             //设置文本大小
             textPaint.setTextSize(getTextSize());
             //设置文本颜色
             textPaint.setColor(getCurrentTextColor());
-            //初始化航速数据
-            lineContents = new String[_layout.getLineCount()];
-            //获得每行数据
-            for (int i = 0; i < _layout.getLineCount(); i++) {
-                int _start = _layout.getLineStart(i);
-                int _end = _layout.getLineEnd(i);
-                lineContents[i] = getText().subSequence(_start, _end).toString();
-            }
+            //获取行数据
+            getTextContentData(_layout);
             //获得行高
             int _lineHeight = -rect.top + rect.bottom;
-            //获得布局大小
-            if (layoutWidth < -2) {
-                layoutWidth = _layout.getWidth();
-            }
-            if(layoutWidth == ViewGroup.LayoutParams.MATCH_PARENT){
-                //重新设置布局尺寸
-                setMeasuredDimension(ViewGroup.LayoutParams.MATCH_PARENT,_lineHeight * _layout.getLineCount());
-            }else if(layoutWidth == ViewGroup.LayoutParams.WRAP_CONTENT){
-                //重新设置布局尺寸
-                setMeasuredDimension(ViewGroup.LayoutParams.WRAP_CONTENT,_lineHeight * _layout.getLineCount());
-            }else{
-                //重新设置布局尺寸
-                setMeasuredDimension(layoutWidth-rect.left,_lineHeight * _layout.getLineCount());
-            }
+            //初始化布局
+            initLayout(_layout);
+            //设置布局区域
+            int[] _area = getWidthAndHeigt(layoutWidth,layoutHeight, _layout.getLineCount(),_lineHeight);
+            //设置布局
+            setMeasuredDimension(_area[0],_area[1]);
+        }
+    }
+
+    /**
+     * 初始化化布局高度
+     * @param _layout
+     */
+    private void initLayout(Layout _layout) {
+        //获得布局大小
+        if (layoutWidth < -2) {
+            layoutWidth = _layout.getWidth();
+        }
+        if(layoutHeight < -2){
+            layoutHeight = _layout.getHeight();
+        }
+    }
+
+    /**
+     * 获取布局数据
+     * @param pWidth
+     * @param pHeight
+     * @return 返回宽高数组
+     */
+    private int[] getWidthAndHeigt(int pWidth, int pHeight,int pLineCount, int pLineHeight){
+        int[] _area = {
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        };
+        //判断布局是否适配
+        if(pWidth == ViewGroup.LayoutParams.MATCH_PARENT && pHeight == ViewGroup.LayoutParams.MATCH_PARENT){
+            _area[0] = ViewGroup.LayoutParams.MATCH_PARENT;
+            _area[1] = ViewGroup.LayoutParams.MATCH_PARENT;
+        }else if(pWidth == ViewGroup.LayoutParams.MATCH_PARENT && pHeight == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            _area[0] = ViewGroup.LayoutParams.MATCH_PARENT;
+            _area[1] = ViewGroup.LayoutParams.WRAP_CONTENT;
+        }else if(pWidth == ViewGroup.LayoutParams.WRAP_CONTENT && pHeight == ViewGroup.LayoutParams.WRAP_CONTENT){
+            _area[0] = ViewGroup.LayoutParams.WRAP_CONTENT;
+            _area[1] = ViewGroup.LayoutParams.WRAP_CONTENT;
+        }else{
+            _area[0] = pWidth-rect.left;
+            _area[1] = pLineHeight * pLineCount;
+        }
+        return _area;
+    }
+
+    /**
+     * 获取行数据
+     * @param _layout 文本布局对象（注：该布局其实使用的是Layout子类对象StaticLayout）
+     */
+    private void getTextContentData(Layout _layout) {
+        //初始化航速数据
+        lineContents = new String[_layout.getLineCount()];
+        //获得每行数据
+        for (int i = 0; i < _layout.getLineCount(); i++) {
+            int _start = _layout.getLineStart(i);
+            int _end = _layout.getLineEnd(i);
+            lineContents[i] = getText().subSequence(_start, _end).toString();
         }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.e(TAG, "onDraw()=======================");
-        Log.e(TAG, "getLineSpacingExtra() = " + getLineSpacingExtra());
-        Log.e(TAG, "getPaint().getFontMetricsInt(null) = " + getPaint().getFontMetricsInt(null));
+        //循环获取每行数据内容
         for (int i = 0; i < lineContents.length; i++) {
+            //获得数据
             String _drawContent = lineContents[i];
             Log.e(TAG, "LINE[" + (i + 1) + "]=" + _drawContent);
+            //绘制每行数据
             canvas.drawText(_drawContent, 0, -rect.top + (-rect.top + rect.bottom) * i, textPaint);
         }
     }
